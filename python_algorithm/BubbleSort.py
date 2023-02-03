fa9adf0e2ea214a32645c88eabe127fd633c45ed
이영재 <youngjae9683@gmail.com>
2023-02-03 15:46:48 +0900

    .

--- python_algorithm/BubbleSort.py
+++ python_algorithm/BubbleSort.py
@@ -1,11 +1,16 @@
 # 1번쨰 칸부터 옆칸과 비교를 진행하며
 # 그 과정이 끝까지 진행되는 정렬
 
+import time
+import random
+
+
 def bubleSort(a, n):
     for i in range(n, 0, -1):
         for j in range(1, i):
             if a[j] > a[j+1]:
                 a[j], a[j+1] = a[j+1], a[j]
+#       print(a)
 
 
 def checkSort(a, n):
@@ -14,22 +19,20 @@ def checkSort(a, n):
         if (a[i] > a[i+1]):
             isSorted = False
         if (not isSorted):
-            break ;
+            break
     if isSorted:
         print(" 정렬완료")
     else:
         print("정렬 오루 발생")
 
-import random , time
 
 N = 5000
 a = []
 a.append(None)
-for i in range (N) :
-    a.append(random.randint(1,N))
+for i in range(N):
+    a.append(random.randint(1, N))
 start_time = time.time()
-bubleSort(a,N)
+bubleSort(a, N)
 end_time = time.time() - start_time
-print("버블 정렬 시간 (N = %d) : %f0.3"%(N,end_time))
-checkSort(a,N)
-
+print("버블 정렬 시간 (N = %d) : %f0.3" % (N, end_time))
+checkSort(a, N)

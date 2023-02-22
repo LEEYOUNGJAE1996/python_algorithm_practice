b8669078e2d8172fad34dd47211904c9d5e541aa
YOUNG <youngjae9683@gmail.com>
2023-02-22 23:13:48 +0900

    행맨 구현 - 강의 코드와 비교 개선점 발견

--- python_study/6day-hangman/hangman.py
+++ python_study/6day-hangman/hangman.py
@@ -82,7 +82,7 @@ life = 5
 
 
 while life != 0:
-    print("your life  >> " + str(life))
+    print(f"your life  >> {life}")
     print(graph(life))
 
     guess = input("Guess a letter: ").lower()
@@ -102,7 +102,16 @@ while life != 0:
     for letter in chosen_word:
         if letter == guess:
             # 중복 비교 제거 + 중복 비교 하는 경우 목숨  -
-            if user_answer[i] == letter:
+            # 내가 구현한 코드
+            # if user_answer[i] == letter:
+            #     print("you already done!")
+            #     continue
+            # else:
+            #     user_answer[i] = letter
+            #     correct = True
+            #  강의에서 제공하는 차이점!
+            #  if문에서도 in 문을 써서 각 내용을 비교 할 수 있다.
+            if guess in user_answer:
                 print("you already done!")
                 continue
             else:
@@ -112,20 +121,28 @@ while life != 0:
 
     # 틀렸을 경우 목숨을 하나씩 줄이는 코드
     if correct == False:
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
         life -= 1
 
     print(user_answer)
 
     # 정답을 다 맞췄는지 비교하는 코드
-    str_answer = ""
-    for letter in user_answer:
-        str_answer += letter
-    if str_answer == chosen_word:
-        print("You Win! ")
+    # 내가 구상한 코드
+    # str_answer = ""
+    # for letter in user_answer:
+    #     str_answer += letter
+    # if str_answer == chosen_word:
+    #     print("You Win! ")
+    #     break
+    # 강의에서 나온 코드  in 이라는 구문을 이용해 좀더 깔끔한 코드를 구성
+    if "_" not in user_answer:
+        print("You win.")
         break
-
 # 목숨이 다 소진한 경우 == 진경우
 if life == 0:
     print(graph(life))
     print("You Lose!")
 print("Game Over")
+
+
+# 강의에서 제공하는 결과물

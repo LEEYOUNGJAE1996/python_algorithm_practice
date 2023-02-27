d6db0a7d95a87def7a7a937e8df3d7f7e8258cf6
YOUNG <youngjae9683@gmail.com>
2023-02-28 01:16:19 +0900

    Dictionary 활용 개선이 필요한 부분

--- python_study/8day-dictionaries활용/exercise_Nesting.py
+++ python_study/8day-dictionaries활용/exercise_Nesting.py
@@ -1,7 +1,7 @@
 # 230227
 # France를 기준으로 cities_visited dictionary 안에 도시들 list형태로
 
-
+# 강의를 듣기전 방식
 cities_visited = {
     "France": ["Prais", "Lille", "Dijon"]
 }
@@ -10,4 +10,15 @@ travel_log = {
     "France": cities_visited["France"]
 }
 
+# 강의에서 한 방식
+# 다른 점
+# France에서 바로 Dictionary형태의 값을 중괄호로 만들어준 후
+# 그 안에서 값을 구분
+# 강의를 보기 전에 나의 경우는 외부에서 또 하나의 Dictionary를 만들어 값을 지정
+# 강의에서 한 방식은 다른 딕션어리를 통해 다른 Key를 계속 추가하기 good
+travel_log_lec = {
+    "France": {"cities_visited": ["Prais", "Lille", "Dijon"], "total_visites": 12}
+}
+
 print(travel_log)
+print(travel_log_lec)

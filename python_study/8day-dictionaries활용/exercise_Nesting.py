96e0b5d3d92ff1b5b588ba851b7d53821eee0c0d
YOUNG <youngjae9683@gmail.com>
2023-02-28 01:18:20 +0900

    Dictionary활용 개선

--- python_study/8day-dictionaries활용/exercise_Nesting.py
+++ python_study/8day-dictionaries활용/exercise_Nesting.py
@@ -17,7 +17,8 @@ travel_log = {
 # 강의를 보기 전에 나의 경우는 외부에서 또 하나의 Dictionary를 만들어 값을 지정
 # 강의에서 한 방식은 다른 딕션어리를 통해 다른 Key를 계속 추가하기 good
 travel_log_lec = {
-    "France": {"cities_visited": ["Prais", "Lille", "Dijon"], "total_visites": 12}
+    "France": {"cities_visited": ["Prais", "Lille", "Dijon"], "total_visites": 12},
+    "Germany": {"cities_visited": ["Berlin", "Hamburg", "Stuttgart"], "total_visites": 3}
 }
 
 print(travel_log)

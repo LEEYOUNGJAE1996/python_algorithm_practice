df4c87e561b8ac52ff0b3258594bec87102c647f
YOUNG <youngjae9683@gmail.com>
2023-03-02 00:20:46 +0900

    calculation 만들기

---
+++ python_study/9day-출력과함수/calculator.py
@@ -0,0 +1,50 @@
+import cal_art
+
+
+def cal(a, b, operation):
+    if operation == "+":
+        return a + b
+    elif operation == "-":
+        return a - b
+    elif operation == "*":
+        return a * b
+    elif operation == "/":
+        return a/b
+    else:
+        return False
+
+# 새로운 수를 ? 아님 기존의 수를?
+
+
+def continueOrNot(input):
+    if input.lower() == 'y':
+        return False
+    elif input.lower() == 'n':
+        return True
+    else:
+        print("you enter worng value. try again")
+        return False
+
+
+print(cal_art.logo)
+
+# start
+newthing = True
+result = 0
+while True:
+    if newthing == True:
+        result = 0
+        fir_number = int(input("What's the first number >> "))
+    print("+")
+    print("-")
+    print("*")
+    print("/")
+    op = input("Pick on operation >> ")
+    sec_number = int(input("What's the next number >> "))
+    result = cal(a=fir_number, b=sec_number, operation=op)
+    if result == False:
+        print("you enter worng value")
+    else:
+        print(f"{fir_number} {op} {sec_number} = {result}")
+    newthing = continueOrNot(input(
+        f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation >>"))

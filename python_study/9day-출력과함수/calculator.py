31598380b231addfab3db1643ac7fb2268c6341c
YOUNG <youngjae9683@gmail.com>
2023-03-02 00:39:34 +0900

    소수점 에러 수정 및 강의 코드 추가

--- python_study/9day-출력과함수/calculator.py
+++ python_study/9day-출력과함수/calculator.py
@@ -43,13 +43,13 @@ should_continue = True
 while should_continue:
     if newthing == True:
         result = 0
-        fir_number = int(input("What's the first number >> "))
+        fir_number = float(input("What's the first number >> "))
     print("+")
     print("-")
     print("*")
     print("/")
     op = input("Pick on operation >> ")
-    sec_number = int(input("What's the next number >> "))
+    sec_number = float(input("What's the next number >> "))
     result = cal(a=fir_number, b=sec_number, operation=op)
     if result == False:
         print("you enter worng value")
@@ -57,3 +57,55 @@ while should_continue:
         print(f"{fir_number} {op} {sec_number} = {result}")
     newthing, should_continue = continueOrNot(input(
         f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation >>"))
+
+
+# 강의 방식 재귀함수를 활용
+#############################################################################
+def add(n1, n2):
+    return n1 + n2
+
+
+def subtract(n1, n2):
+    return n1 - n2
+
+
+def multiply(n1, n2):
+    return n1 * n2
+
+
+def divide(n1, n2):
+    return n1 / n2
+
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+
+def calculator():
+    print(cal_art.logo)
+
+    num1 = float(input("What's the first number?: "))
+    for symbol in operations:
+        print(symbol)
+    should_continue = True
+
+    while should_continue:
+        operation_symbol = input("Pick an operation: ")
+        num2 = float(input("What's the next number?: "))
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == 'y':
+            num1 = answer
+        else:
+            should_continue = False
+            calculator()
+
+
+calculator()
+###############################################################################################

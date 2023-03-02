bb0f35338a638a0b383aed3db95844c4a18beff6
YOUNG <youngjae9683@gmail.com>
2023-03-03 00:53:22 +0900

    메인은 간단히

--- python_study/11day-scope/숫자맞추기게임.py
+++ python_study/11day-scope/숫자맞추기게임.py
@@ -17,35 +17,39 @@ def game(life):
         return 0
 
 
-print(art.logo)
-print("Welcome to the Number Guessing Game!")
-print("I'm thinking of a number between 1 and 100.")
-life = 0
-level_choice = True
-while level_choice:
-    level = input("Choose a difficulty. Type 'easy' or 'hard' : ").lower()
-    if level == "hard":
-        life = 5
-        level_choice = False
-    elif level == "easy":
-        life = 10
-        level_choice = False
-    else:
-        print("you entered worng value.")
-        level_choice = True
-for i in range(life, 0, -1):
-    result = game(i)
-    if result == 2:
-        print("Too high")
-    elif result == 1:
-        print("Too low")
-    else:
-        break
-    print("Guess again")
-    life = i - 1
+def start_game():
+    print(art.logo)
+    print("Welcome to the Number Guessing Game!")
+    print("I'm thinking of a number between 1 and 100.")
+    life = 0
+    level_choice = True
+    while level_choice:
+        level = input("Choose a difficulty. Type 'easy' or 'hard' : ").lower()
+        if level == "hard":
+            life = 5
+            level_choice = False
+        elif level == "easy":
+            life = 10
+            level_choice = False
+        else:
+            print("you entered worng value.")
+            level_choice = True
+    for i in range(life, 0, -1):
+        result = game(i)
+        if result == 2:
+            print("Too high")
+        elif result == 1:
+            print("Too low")
+        else:
+            break
+        print("Guess again")
+        life = i - 1
 
-print(f"Random number is {RANDOM_NUMBER}")
-if life != 0:
-    print("You win")
-else:
-    print("You lose")
+    print(f"Random number is {RANDOM_NUMBER}")
+    if life != 0:
+        print("You win")
+    else:
+        print("You lose")
+
+
+start_game()

3764537e51389f4d8b0c266f015c141c407131e8
YOUNG <youngjae9683@gmail.com>
2023-03-06 20:39:54 +0900

    table 객체 사용해보기

--- python_study/15day-OOP/main.py
+++ python_study/15day-OOP/main.py
@@ -1,11 +1,20 @@
 # 230306
 
-from turtle import Turtle, Screen
-timmy = Turtle()
-print(timmy)
-timmy.shape("turtle")
-timmy.color("lightblue")
-timmy.forward(100)
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+# from turtle import Turtle, Screen
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("lightblue")
+# timmy.forward(100)
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+
+from prettytable import PrettyTable
+
+table = PrettyTable()
+table.add_column("Pockemon Name", ["r", "s", "d"])
+table.add_column("TYPE", ["water", "fire", "wind"])
+
+print(table)

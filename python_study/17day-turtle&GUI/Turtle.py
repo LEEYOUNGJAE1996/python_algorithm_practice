12beabb15e0f164f1a62abae9f8fb0b23a4389f2
YOUNG <youngjae9683@gmail.com>
2023-03-08 21:13:28 +0900

    turtle 예시

---
+++ python_study/17day-turtle&GUI/Turtle.py
@@ -0,0 +1,12 @@
+# 230308
+
+from turtle import Turtle, Screen
+
+
+# screen = Screen()
+# screen.exitonclick()
+
+
+timmy_turtle = Turtle()
+timmy_turtle.shape("turtle")
+timmy_turtle.color("blue")

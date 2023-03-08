d6af66eed7ac585cb60e59a0258d1cb50aada281
YOUNG <youngjae9683@gmail.com>
2023-03-08 21:37:03 +0900

    turtle 활용

--- python_study/17day-turtle&GUI/Turtle.py
+++ python_study/17day-turtle&GUI/Turtle.py
@@ -9,4 +9,13 @@ from turtle import Turtle, Screen
 
 timmy_turtle = Turtle()
 timmy_turtle.shape("turtle")
-timmy_turtle.color("blue")
+timmy_turtle.color("blueViolet")
+for i in range(4):
+    timmy_turtle.forward(100)
+    timmy_turtle.right(90)
+
+for i in range(10):
+    timmy_turtle.forward(10)
+    timmy_turtle.penup()
+    timmy_turtle.forward(10)
+    timmy_turtle.pendown()

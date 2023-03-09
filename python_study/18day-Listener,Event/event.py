57358a958c11f9f8507bc5535cdafd5f27377bdf
YOUNG <youngjae9683@gmail.com>
2023-03-09 23:33:23 +0900

    Event and listener

---
+++ python_study/18day-Listener,Event/event.py
@@ -0,0 +1,19 @@
+# 230309
+
+from turtle import Turtle, Screen
+
+young = Turtle()
+screen = Screen()
+
+
+def move_forwards():
+    young.forward(10)
+
+
+screen.listen()
+# space키를 누르면 , 뒤에 구문을 실행
+# 주의 fun에 입력하는 경우 함수의 ()를 입력하면 받아들이지 못함
+# ()를 제외한 상태로 입력
+screen.onkey(key="space", fun=move_forwards)
+
+screen.exitonclick()
